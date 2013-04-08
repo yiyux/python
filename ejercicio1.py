1f4ecdc9dd061c01cc62701396fdd1afb456e2fc
kfischer <karl.fischer.w@gmail.com>
2013-04-07 22:58:29 -0400

    Se agregan archivos de ejercicios y libros

--- ejercicio1.py
+++ ejercicio1.py
@@ -1,6 +1,40 @@
 __author__ = 'yiyux'
 
-print "Hola Mundo"
+# Tarea, teniendo la siguiente pista
+# K -> M
+# O -> Q
+# E -> G
+# que significa el siguiente texto
 
-a = input("Ingrese un numero?")
-print pow(a,a)
+# g fmnc wms bgblr rpylqjyrc gr zw fylb. rfyrq ufyr amknsrcpq ypc dmp.
+# bmgle gr gl zw fylb gq glcddgagclr ylb rfyr'q ufw rfgq rcvr gq qm jmle.
+# sqgle qrpgle.kyicrpylq() gq pcamkkclbcb. lmu ynnjw ml rfc spj.
+
+def buscarLetra(letraBuscar, diccionario):
+    for letra in diccionario:
+        if letra == letraBuscar:
+            encontrado = True
+            break
+        else:
+            encontrado = False
+    return encontrado
+
+
+diccionario = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r",
+               "s", "t", "u", "v", "w", "x", "y", "z"]
+salto = 2
+#print diccionario.index("c")
+texto = raw_input("ingrese texto?")
+palabra = ""
+for letra in texto:
+    if buscarLetra(letra, diccionario):
+        indice = diccionario.index(letra) + salto
+        if indice > (len(diccionario) - 1):
+            indice -= len(diccionario)
+        palabra += diccionario[indice]
+        #print diccionario.index(letra),
+    else:
+        palabra += letra
+
+print palabra
+
